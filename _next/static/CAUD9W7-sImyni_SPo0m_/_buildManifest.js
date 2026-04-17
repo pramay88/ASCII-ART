67c65fe0f86f52d75324755bb6cdf79eb0962642
Pramay Wankhade <pramaywankhade7@gmail.com>
2026-04-17 16:36:13 +0530

    Updates

---
+++ _next/static/CAUD9W7-sImyni_SPo0m_/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/ASCII-ART//_next/:path+",
+        "destination": "/ASCII-ART/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
